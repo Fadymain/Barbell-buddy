a3757a7707d4d70d18c02273299cae69725b8d2a
Your Name <your@email.com>
2022-04-20 16:54:46 -0400

    add code to Button component

---
+++ react-front-end/src/components/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import "components/Button.scss";
+// import classNames from "classnames";
+
+export default function Button(props) {
+  // const buttonClass = classNames("button", {
+  //   "button--confirm": props.confirm,
+  //   "button--danger": props.danger
+  // });
+ 
+  return (
+    <button
+      onClick={props.onClick}
+      // className={buttonClass}
+      // disabled={props.disabled}
+    >
+      {props.children}
+      Hello!
+    </button>
+  );
+}
