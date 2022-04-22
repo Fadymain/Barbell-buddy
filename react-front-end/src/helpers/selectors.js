f1579c92bd0a098423049f6fdd0a0b46902ad045
Adetola Fadairo <adetolafadairo@gmail.com>
2022-04-22 22:08:04 +0000

    creating helper functions

---
+++ react-front-end/src/helpers/selectors.js
@@ -0,0 +1,13 @@
+//getExercisesForDay get exercises done for the day by the user 
+//getExercisesForUser gets exercises selected by the user
+
+export function getExercisesForDay() {
+  const completedExercise = [];
+}
+
+export function getExercisesForUser() {
+  const userExercise = [];
+
+
+
+}
