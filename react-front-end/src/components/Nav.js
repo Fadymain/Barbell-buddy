a9cf8667637eab039ad9c63bd41ac58ee1c775aa
Your Name <your@email.com>
2022-04-25 16:44:45 -0400

    basic appBar component

--- react-front-end/src/components/Nav.js
+++ react-front-end/src/components/Nav.js
@@ -1,41 +1,35 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import { AppBar, Toolbar, makeStyles, Typography } from "@material-ui/core";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
+const useStyles = makeStyles({
+  header: {
+    backgroundColor: "DeepSkyBlue",
+    color: "black",
+    boxShadow: "0px 0px 0px 0px",
+  },
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  }
+});
 
 const Nav = () => {
 
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  const classes = useStyles();
 
   return (
-    
-<AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" color="inherit" aria-label="menu">
-      {/* <img /> */}
-    </IconButton>
-    <Typography variant="h6" >
-      Barbell Buddy
-    </Typography>
-    <AccountCircle />
-  </Toolbar>
-</AppBar>
+
+    <div className="App">
+      <AppBar position="sticky" className={classes.header}>
+        <Toolbar>
+          <Typography variant="h6"> Barbell Buddy </Typography>
+          {/* <AccountCircleIcon /> */}
+        </Toolbar>
+      </AppBar>
+    </div>
 
   );
 
