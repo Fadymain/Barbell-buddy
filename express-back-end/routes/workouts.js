5693689f7b24affc4151866aecacea16c6217df6
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-04-29 22:21:38 +0000

    fixed backend adding working route

--- express-back-end/routes/workouts.js
+++ express-back-end/routes/workouts.js
@@ -33,30 +33,37 @@ module.exports = (db) => {
   });
 
   router.post("/add", (req, res) => {
-    const {user_id, day_at, exercise_id, reps, weight} = req.body
+    const {users_id, day_at, exercises_id, reps, weight} = req.body
+    console.log("add workout route", req.body);
+    
      if (day_at){
-      db.query(`INSERT INTO workouts (user_id, day_at, exercise_id, reps, weight) VALUES ($1, $2, $3, $4, $5) RETURNING *;`, [user_id, day_at, exercise_id, reps, weight])
+      db.query(`INSERT INTO workouts (users_id, day_at, exercises_id, reps, weight) VALUES ($1, $2, $3, $4, $5) RETURNING *;`, [users_id, day_at, exercises_id, Number(reps), Number(weight)])
         .then(data => {
           const regWorkout = data.rows[0];
           console.log("created workout", regWorkout);
           res.status(200).json({regWorkout: regWorkout});
-        });
+        }).catch((err) =>{
+          console.log("exercises_id", exercises_id);
+          console.log("workoutAdd error", err.message);
+        })
      } else {
-      db.query(`INSERT INTO workouts (user_id, exercise_id, reps, weight) VALUES ($1, $2, $3, $4) RETURNING *;`, [user_id, exercise_id, reps, weight])
+      db.query(`INSERT INTO workouts (users_id, exercises_id, reps, weight) VALUES ($1, $2, $3, $4) RETURNING *;`, [users_id, exercises_id, Number(reps), Number(weight)])
       .then(data => {
         const regWorkout = data.rows[0];
         console.log("created workout", regWorkout);
         res.status(200).json({regWorkout: regWorkout});
-      });
+      }).catch((err) =>{
+        console.log("workoutAdd error", err.message);
+      })
      }
     
 
   });
 
   router.post("/update", (req, res) => {
-    const {user_id, day_at, exercise_id, reps, weight} = req.body
+    const {users_id, day_at, exercises_id, reps, weight} = req.body
      if (day_at){
-      db.query(`UPDATE workouts SET exercise_id = $1, reps = $2, weight = $3 WHERE user_id = $4 AND day_at = $5`, [exercise_id, reps, weight,user_id, day_at])
+      db.query(`UPDATE workouts SET exercises_id = $1, reps = $2, weight = $3 WHERE users_id = $4 AND day_at = $5`, [exercises_id, reps, weight,users_id, day_at])
         .then(data => {
           const regWorkout = data.rows;
           console.log("updated workout", regWorkout);
